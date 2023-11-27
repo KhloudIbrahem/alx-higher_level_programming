b94cba6d9c0a7b6574b2288b7c5a4b977d95149e
Khloudibrahem <Khloudk130@gmail.com>
2023-11-27 10:17:21 -0800

    Task 4

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Define Attributes of rectangle"""
+
+
+class Rectangle:
+    """Implements the rectangle class"""
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle attributes
+        Args:
+        width (int): the width
+        height (int): the height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the rectangle area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """returns the rectangle perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return ((self.width + self.height) * 2)
+
+    def __str__(self):
+        """Print the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return ((("#" * self.width) + "\n") * self.height)[:-1]
+
+    def __repr__(self):
+        """Print the rectangle using eval."""
+        return "Rectangle({}, {})".format(self.width, self.height)
