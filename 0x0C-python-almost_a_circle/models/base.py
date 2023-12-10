7bd08063b1bc8b2ab73e28f39407b8c5eae28ca2
KhloudIbrahem <Khloudk130@gmail.com>
2023-12-10 11:07:42 -0800

    Task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,12 @@ class Base:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 f.write(Base.to_json_string(list_dicts))
 
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string
+        Args:
+        json_string is a string representing a list of dictionaries
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
