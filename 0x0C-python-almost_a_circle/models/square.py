fc11349d3bbcdf6901adf4604b1cb36f69392f53
KhloudIbrahem <Khloudk130@gmail.com>
2023-12-10 10:13:43 -0800

    Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,5 @@ class Square(Rectangle):
         """return [Square] (<id>) <x>/<y> - <size>"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                 self.width)
+
+
