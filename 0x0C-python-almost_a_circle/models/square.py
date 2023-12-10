c64f5995dea2037309829a71a6d05ba7d0d1b5ee
KhloudIbrahem <Khloudk130@gmail.com>
2023-12-10 10:07:43 -0800

    Task 9

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Define the square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent the Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize the square attributes
+        Args:
+        size (int): the size length.
+        x (int): the coordinate x.
+        y (int): the coordinate y.
+        id (int): the identity of the square.
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """retrieve it"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set it"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """return [Square] (<id>) <x>/<y> - <size>"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                self.width)
